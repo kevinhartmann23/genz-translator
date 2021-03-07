592fbc7c4d42ec7245918df0f7aa06263152faa0
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-03-06 19:59:34 -0700

    Add emoji dummy data

---
+++ src/components/EmojiGuide/emojiData.js
@@ -0,0 +1,42 @@
+const emojiData = [
+  {
+    emoji: '⏳',
+    definition: 'attractive'
+  },
+  {
+    emoji: '🍁',
+    definition: 'marijuana'
+  },
+  {
+    emoji: '🤡',
+    definition: 'foolish'
+  },
+  {
+    emoji: '🥺',
+    definition: 'cute'
+  },
+  {
+    emoji: '😭',
+    definition: 'laughter'
+  },
+  {
+    emoji: '👁👄👁',
+    definition: 'shock'
+  },
+  {
+    emoji: '💀',
+    definition: 'hilarious'
+  },
+  {
+    emoji: '🔥',
+    definition: 'really good'
+  },
+  {
+    emoji: '🙂',
+    definition: 'passive-aggressive'
+  },
+  {
+    emoji: '🤧',
+    definition: 'frustration'
+  },
+]
