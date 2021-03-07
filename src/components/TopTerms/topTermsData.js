148369c022d8aae9b5acecd256d91d7fc8189a47
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-03-06 20:54:30 -0700

    Add topterms

--- src/components/TopTerms/topTermsData.js
+++ src/components/TopTerms/topTermsData.js
@@ -1,43 +1,43 @@
 const topTermsData = [
   {
     word: 'Cancel Culture',
-    defintion: 'a form of online shaming to express disappointment in the views or actions of a public figure, company, or organization.',
+    definition: 'a form of online shaming to express disappointment in the views or actions of a public figure, company, or organization.',
   },
   {
     word: 'CEO',
-    defintion: 'Playing on the definition of CEO in real life, when someone is the “CEO of” something, it means they are the very best at it or have mastered it.',
+    definition: 'Playing on the definition of CEO in real life, when someone is the “CEO of” something, it means they are the very best at it or have mastered it.',
   },
   {
     word: 'Fam',
-    defintion: 'Fam is used the way our generation may have used “bro.” This term is used for your closest of friends.',
+    definition: 'Fam is used the way our generation may have used “bro.” This term is used for your closest of friends.',
   },
   {
     word: 'Glow Up',
-    defintion: 'Glow up describes a makeover or transformation from bad to good in some way. If you think a new haircut makes you more attractive, you might refer to this as a “glow up.”',
+    definition: 'Glow up describes a makeover or transformation from bad to good in some way. If you think a new haircut makes you more attractive, you might refer to this as a “glow up.”',
   },
   {
     word: 'Woke',
-    defintion: 'Woke refers to political awareness. People who are woke are not politically incorrect. They care about having an open mind and things like the earth, Black Lives Matter, and feminism.',
+    definition: 'Woke refers to political awareness. People who are woke are not politically incorrect. They care about having an open mind and things like the earth, Black Lives Matter, and feminism.',
   },
   {
     word: 'TFW',
-    defintion: 'TFW is an abbreviation for “that feeling when” as in “TFW you have a hot cup of coffee in a quiet house.”',
+    definition: 'TFW is an abbreviation for “that feeling when” as in “TFW you have a hot cup of coffee in a quiet house.”',
   },
   {
     word: 'Sip Tea',
-    defintion: 'Sip tea is a phrase that is used when you are just sitting back and listening to the gossip as opposed to participating in it.',
+    definition: 'Sip tea is a phrase that is used when you are just sitting back and listening to the gossip as opposed to participating in it.',
   },
   {
     word: 'Snack',
-    defintion: 'A snack is a term you might use to describe a person that you find attractive.',
+    definition: 'A snack is a term you might use to describe a person that you find attractive.',
   },
   {
     word: 'Salty',
-    defintion: 'If you feel salty, it means that you are jealous.',
+    definition: 'If you feel salty, it means that you are jealous.',
   },
   {
     word: 'Finna',
-    defintion: 'Finna is short for saying “I am going to.” You could say, “I am finna order some pizza.”',
+    definition: 'Finna is short for saying “I am going to.” You could say, “I am finna order some pizza.”',
   }
 ]
 
