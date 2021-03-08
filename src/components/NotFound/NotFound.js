1bf596331ef999a54b4b0783efd88b86f6907f6f
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-03-08 15:43:36 -0700

    Add testing for error page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -20,14 +20,16 @@ function NotFound() {
       {currentUser && 
         <Window style={{ width: '80%', height: '90%', marginTop: '3.5rem' }}>
           <WindowHeader active={false} className='window-header' style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-            <span>404! </span>
+            <span>404!</span>
           </WindowHeader>
-          <WindowContent style={{display:'flex', flexDirection:'column', justifyContent:'space-between', width:'95%', height:'90%', alignItems:'center'}}>
-          <img style={{ height: '20rem', width: '20rem'}} src={notFound} alt='Error' />
-            <p className='title-notfound'style={{textAlign: 'center'}}>Uh Oh! Looks like we can't find the page you are looking for or an error has occured!</p>
+          <WindowContent style={{display:'flex', flexDirection:'column', justifyContent:'space-around', width:'95%', height:'90%', alignItems:'center'}}>
+            <div style={{ width: '100%', height:'75%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <img style={{ height: '15rem', width: '15rem'}} src={notFound} alt='Error' />
+              <p className='title-notfound'style={{textAlign: 'center'}}>Uh Oh! Looks like we can't find the page you are looking for or an error has occured!</p>
+            </div>
             <div style={{width:'100%', display:'flex', flexDirection:'column', alignItems:'center'}}>
               <Link to='/' style={{ width: '2rem' }}>
-                <Button style={{ fontWeight: 'bold', fontSize:'2rem', width:'20rem', height:'5rem'}}>
+                <Button className='dashboard-button' style={{ fontWeight: 'bold', fontSize:'2rem', width:'20rem', height:'3rem'}}>
                   ↩︎ To Dashboard
                 </Button>
               </Link>
