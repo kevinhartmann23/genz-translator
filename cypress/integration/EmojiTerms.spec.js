3fcd8899e92aa004b2ed5d8dc5cd46c67050118c
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-03-08 14:23:45 -0700

    Add testing for emoji guide and hip terms

--- cypress/integration/EmojiTerms.spec.js
+++ cypress/integration/EmojiTerms.spec.js
@@ -1,42 +1,55 @@
-const baseUrl = 'http://localhost:3000/login'
+const baseUrl = 'http://localhost:3000/'
 const userEmail = 'test@test.com'
 const userPassword = 'testing'
 
-describe('DESCRIBE', () => {
+describe('Hip Terms', () => {
   it('Should visit search path on clicking the search icon', () => {
     cy
-      .visit(baseUrl)
+      .visit(`${baseUrl}login`)
       .get('.email').type(userEmail)
       .get('.password').type(userPassword)
       .get('.form-submit-button').click()
   })
 
-  it('Should', () => {
-
+  it('Should redirect user to hip terms after clicking the icon', () => {
+    cy 
+      .get('#top-terms').click()
+      .url().should('eq', `${baseUrl}hipterms`)
   })
 
-  it('Should', () => {
-
+  it('Should display top terms', () => {
+    cy 
+      .get('td:first').should('have.text', 'Cancel Culture:')
+      .get('td').eq(1).should('have.text', 'a form of online shaming to express disappointment in the views or actions of a public figure, company, or organization.')
   })
 
-  it('Should', () => {
-
+  it('Should redired user back to dashboard when clicking exit', () => {
+    cy 
+      .get('.exit-button').click()
+      .url().should('eq', baseUrl)
   })
 
-  it('Should', () => {
+})
 
+describe('Emoji Guide', () => {
+  it('Should direct user to emoji guide when clicking the emoji guid icon', () => {
+    cy 
+      .get('.sidebar')
+      .get('#emoji-guide').click()
+      .url().should('eq', `${baseUrl}emojiguide`)
   })
 
-  it('Should', () => {
-
+  it('Should display an emoji with its meaning', () => {
+    cy
+      .get('td:first').should('have.text', '⏳')
+      .get('td').eq(1).should('have.text', 'attractive')
   })
-
+  
   it('Should allow user to logout at anytime', () => {
     cy
       .get('.start-button').click()
       .get('.logout-button').click()
 
-      .url().should('eq', baseUrl)
+      .url().should('eq', `${baseUrl}login`)
   })
-
 })
